3278d4db02d61de8d54adeb32edc6b2cf9bec74d
parashut <parashut@gmail.com>
2017-12-13 15:53:44 +0100

    +sessions

--- src/main/java/com/example/websocket/Websocket.java
+++ src/main/java/com/example/websocket/Websocket.java
@@ -2,16 +2,22 @@ package com.example.websocket;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.BinaryMessage;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.handler.BinaryWebSocketHandler;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration //zeby klasa zamienila sie w beana
 @EnableWebSocket
 public class Websocket extends BinaryWebSocketHandler implements WebSocketConfigurer {
 
+    private Map<String, WebSocketSession> sessions = Collections.synchronizedMap(new HashMap<String, WebSocketSession>());
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
@@ -21,10 +27,21 @@ public class Websocket extends BinaryWebSocketHandler implements WebSocketConfig
     @Override
     protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws Exception {
         System.out.println("Wiadomość przychodząca: " + new String(message.getPayload().array()));
+
+        for (WebSocketSession userSesion : sessions.values()) {
+            userSesion.sendMessage(message);
+        }
     }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        System.out.println("Nawiązano polaczenie");
+        sessions.put(session.getId(), session);
+        System.out.println("Nowy użytkownik zarejestrowany");
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session.getId());
+        System.out.println("Użytkownik wyrejestrowany");
     }
 }
